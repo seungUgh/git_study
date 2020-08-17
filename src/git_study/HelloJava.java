3ec3951df1ef61559aaf7763503ee7fa373250fa
seungeun <tmddms0931@naver.com>
2020-08-17 11:07:26 +0900

    .

--- src/git_study/HelloJava.java
+++ src/git_study/HelloJava.java
@@ -4,5 +4,6 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("World's end Dancehall");
+		System.out.println("Hello Java");
 	}	
 }
