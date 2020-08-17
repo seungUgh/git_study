09bf622703ddabb8ed2338f0e226b7400ce147f3
seungeun <tmddms0931@naver.com>
2020-08-17 11:06:47 +0900

    add

--- src/git_study/HelloJava.java
+++ src/git_study/HelloJava.java
@@ -1,7 +1,9 @@
 package git_study;
 
 public class HelloJava {
-public static void main(String[] args) {
-	System.out.println("Hello World");
-}
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("World's end Dancehall");
+		System.out.println("Earth's End Dancehall");
+	}
 }
